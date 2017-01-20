569145345abbe4208c1c5f37387a795d09880b39
Julian Neuberger <neuberger.julian@gmail.com>
2017-01-20 22:16:00 +0100

    First iteration of Sonar Pings :+1:

---
+++ BoatWave/Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.A))
+        {
+            Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
+            rb.AddForce(Vector2.left);
+        }
+	}
+}
---
+++ BoatWave/Assets/SonarFallback.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonarFallback : MonoBehaviour
+{
+    // ##################################################
+    // vars that can be changed from inspector          #
+    // ##################################################
+
+    public float SonarMaxRange      = 50;
+    // in units/second
+    public float SonarSpeed         = 10; 
+
+    // ##################################################
+    // vars hidden from inspector                       #
+    // ##################################################
+
+    protected Light Sonar;
+    protected bool Pinging;
+    protected float SonarIntensity;
+    float IntensityChange;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Sonar = GetComponent<Light>();
+        SonarIntensity = Sonar.intensity;
+        IntensityChange = SonarIntensity / (SonarMaxRange / SonarSpeed);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(!Pinging && Input.GetKeyDown(KeyCode.E))
+        {
+            Pinging = true;
+        }
+        if(Pinging)
+        {
+            if(Sonar.range >= SonarMaxRange)
+            {
+                Pinging = false;
+                Sonar.range = 0;
+                Sonar.intensity = SonarIntensity;
+            }
+            else
+            {
+                Sonar.range += Time.deltaTime * SonarSpeed;
+                Sonar.intensity -= IntensityChange * Time.deltaTime;
+            }
+        }
+	}
+}

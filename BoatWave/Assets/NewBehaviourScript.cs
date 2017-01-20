044e1753d7b9d6f8c30e6924aa3da70ccb14cea5
Nicolas Hilberg <nicolas.hilberg@gmail.com>
2017-01-20 21:34:41 +0100

    Movement Behaviour

---
+++ BoatWave/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    Vector3 speed = new Vector3();
+    float acc = 0f;
+    void Update()
+    {
+        var acc = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        addforce(acc);
+        transform.position += speed * Time.deltaTime;
+    }
+
+    void addforce(Vector3 acc)
+    {
+        speed = speed + acc;
+
+    }
+}

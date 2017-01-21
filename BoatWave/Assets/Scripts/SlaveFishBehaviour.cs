5e2f3a0a5e5c91cf580b00ec20a52171ea75426d
Julian Neuberger <neuberger.julian@gmail.com>
2017-01-21 21:40:12 +0100

    minor

---
+++ BoatWave/Assets/Scripts/SlaveFishBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlaveFishBehaviour : MonoBehaviour {
+
+    protected GameObject leader;
+    protected Vector2 offset;
+    new protected Rigidbody2D rigidbody;
+
+    // Use this for initialization
+    void Start ()
+    {
+        leader = transform.parent.gameObject;
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 dir = leader.transform.position - transform.position + new Vector3(offset.x, offset.y, 0);
+        rigidbody.velocity = dir;
+	}
+
+    void SetOffset(Vector2 offset)
+    {
+        this.offset = offset;
+    }
+}

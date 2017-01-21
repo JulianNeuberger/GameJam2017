3206a7ba7a89a6a51ee51fefa24d953349af06bd
Nicolas Hilberg <nicolas.hilberg@gmail.com>
2017-01-21 17:52:06 +0100

    Minor!

--- BoatWave/Assets/RandomDistortion.cs
+++ BoatWave/Assets/RandomDistortion.cs
@@ -6,17 +6,17 @@ public class RandomDistortion : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        transform.Find("LightningDistortion").gameObject.SetActive(false);
+       
     }
 	
 	// Update is called once per frame
 	void Update () {
         float r = Random.value;
         if (r < 0.5)
-            transform.Find("LightningDistortion").gameObject.SetActive(false);
+            transform.Find("LightningDistortionRandom").gameObject.SetActive(false);
         if (r < 0.01)
         {
-            transform.Find("LightningDistortion").gameObject.SetActive(true);
+            transform.Find("LightningDistortionRandom").gameObject.SetActive(true);
         }
        
     }

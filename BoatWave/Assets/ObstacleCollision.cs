ece515e63e63d8eca7101d4bbbd4df543b67b35a
Digitale Medien <Digitale Medien>
2017-01-21 15:10:22 +0100

    Statische Hindernisse und Schadenskript

---
+++ BoatWave/Assets/ObstacleCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleCollision : MonoBehaviour {
+
+    // Use this for initialization
+
+    public float obstacleDamage = 10;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) 
+	{
+		if(coll.gameObject.tag == "Player")
+		{
+            Debug.Log("Zusammenstoß");
+			GameObject player = coll.gameObject;
+            player.GetComponent<HealtSystemShip>().TakeDamage(obstacleDamage);
+		}
+	}
+
+}

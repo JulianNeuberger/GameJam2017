beee9753bc90631a39dd911fb2543c455dcad8a6
Julian Neuberger <neuberger.julian@gmail.com>
2017-01-21 12:43:18 +0100

    First iteration of mines, dont do damage yet, waiting for damage system

---
+++ BoatWave/Assets/MineBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineBehaviour : MonoBehaviour {
+
+    protected new ParticleSystem particleSystem;
+    protected bool alive = true;
+    protected Color color;
+
+    // Use this for initialization
+    void Start ()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        color = gameObject.GetComponent<SpriteRenderer>().material.color;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!this.alive)
+        {
+            if(!particleSystem.isPlaying)
+            {
+                Destroy(this.gameObject);
+            }
+            color = gameObject.GetComponent<SpriteRenderer>().material.color;
+            if (color.a > 0)
+            {
+                gameObject.GetComponent<SpriteRenderer>().material.color 
+                    = new Color(color.r, color.g, color.b, color.a - .1f);
+            }
+        }
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(particleSystem != null)
+        {
+            particleSystem.Play();
+            // no more collisions
+            GetComponent<Rigidbody2D>().simulated = false;
+            alive = false;
+        }
+    }
+}

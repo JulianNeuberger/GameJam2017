593e5ae65cf9072fc1f9252174fb07ac272e8fbc
Julian Neuberger <neuberger.julian@gmail.com>
2017-01-21 17:31:49 +0100

    Tethered Camera is now really tethered!

--- BoatWave/Assets/Scripts/TetheredCamera.cs
+++ BoatWave/Assets/Scripts/TetheredCamera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TetheredCamera : MonoBehaviour {
 
     public GameObject player;
+    public float tetherStrength = 100;
 
 	// Use this for initialization
 	void Start () {
@@ -13,8 +14,17 @@ public class TetheredCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+	}
+
+    void LateUpdate()
+    {
         Vector3 playerPos = player.transform.position;
         playerPos.z = -10;
-        this.transform.position = playerPos;
-	}
+        Vector3 offset = playerPos - this.transform.position;
+        if(offset.magnitude > .1)
+        {
+            this.transform.position += offset / this.tetherStrength;
+        } 
+    }
 }

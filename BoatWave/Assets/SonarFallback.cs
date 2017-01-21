ae65695efbc38ff7b15b75d31b2fcff7fd9c18c3
Julian Neuberger <neuberger.julian@gmail.com>
2017-01-21 01:02:00 +0100

    Refactoring

--- BoatWave/Assets/SonarFallback.cs
+++ BoatWave/Assets/SonarFallback.cs
@@ -10,8 +10,7 @@ public class SonarFallback : MonoBehaviour
 
     public float SonarMaxRange      = 50;
     // in units/second
-    public float SonarSpeed         = 10;
-    public float InitialRange       = 0;
+    public float SonarSpeed         = 10; 
 
     // ##################################################
     // vars hidden from inspector                       #
@@ -21,17 +20,18 @@ public class SonarFallback : MonoBehaviour
     protected bool Pinging;
     protected float SonarIntensity;
     protected GameObject SonarEffect;
+    protected Color SonarColor;
     protected float IntensityChange;
-    public Color SonarColor;
 
     // Use this for initialization
     void Start ()
     {
-        Sonar = this.gameObject.transform.Find("Sonar").GetComponent<Light>();
+        Sonar = gameObject.transform.Find("Sonar").GetComponent<Light>();
         SonarIntensity = Sonar.intensity;
         IntensityChange = SonarIntensity / (SonarMaxRange / SonarSpeed);
-        SonarEffect = Sonar.transform.Find("SonarEffect").gameObject;
-        SonarColor = SonarEffect.GetComponent<SpriteRenderer>().material.color;
+        SonarEffect = Sonar.gameObject.transform.Find("SonarEffect").gameObject;
+        SonarColor = SonarEffect.GetComponent<SpriteRenderer>().color;
+        SonarEffect.transform.localScale = new Vector3(0, 0, 0);
     }
 	
 	// Update is called once per frame
@@ -41,29 +41,40 @@ public class SonarFallback : MonoBehaviour
         {
             Pinging = true;
         }
-        if(Pinging)
+        if (Pinging)
         {
-            if(Sonar.range >= SonarMaxRange)
-            {
-                Pinging = false;
-                Sonar.range = 0;
-                Sonar.intensity = SonarIntensity;
-                SonarEffect.transform.localScale = new Vector3(0, 0, 0);
-            }
-            else
-            {
-                // sonar "point light"
-                Sonar.range += Time.deltaTime * SonarSpeed;
-                Sonar.intensity -= Time.deltaTime * IntensityChange;
-
-                // ### sonar effect
-                // sonar effect gets larger
-                SonarEffect.transform.localScale = new Vector3(Sonar.range, Sonar.range, 1);
-                // sonar effects gets more transparent
-                float SonarColorAlpha = (1 - Sonar.range / SonarMaxRange) * .5f;
-                SonarEffect.GetComponent<SpriteRenderer>().material.color = 
-                    new Color(SonarColor.r, SonarColor.g, SonarColor.b, SonarColorAlpha);
-            }
+            UpdateSonar();
         }
 	}
+
+    protected void UpdateSonar()
+    {
+        if (Sonar.range >= SonarMaxRange)
+        {
+            // reset the sonar light and effect to inital values
+            ResetSonar();
+        }
+        else
+        {
+            // sonar "point light"
+            Sonar.range += Time.deltaTime * SonarSpeed;
+            Sonar.intensity -= Time.deltaTime * IntensityChange;
+
+            // ### sonar effect
+            // sonar effect gets larger
+            SonarEffect.transform.localScale = new Vector3(Sonar.range, Sonar.range, 1);
+            // sonar effects gets more transparent
+            float SonarColorAlpha = (1 - Sonar.range / SonarMaxRange) * .25f;
+            SonarEffect.GetComponent<SpriteRenderer>().material.color =
+                new Color(SonarColor.r, SonarColor.g, SonarColor.b, SonarColorAlpha);
+        }
+    }
+
+    protected void ResetSonar()
+    {
+        Pinging = false;
+        Sonar.range = 0;
+        Sonar.intensity = SonarIntensity;
+        SonarEffect.transform.localScale = new Vector3(0, 0, 0);
+    }
 }
